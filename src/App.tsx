ca584746e3a5b932f8cfff5cb4bfad31a88441e0
tarif66 <65169980+tarif66@users.noreply.github.com>
2024-02-20 10:17:00 +0100

    added MUI but not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, useState, useEffect } from 'react'
-import Fec from './models/fec'
-import FECS from './models/mock-fec'
+import Fec from './models/fec';
+import FECS from './models/mock-fec';
+
+
 
 
 const App: FunctionComponent = () => {
@@ -11,15 +13,29 @@ const App: FunctionComponent = () => {
 }, []);  
   
   
-
   return (
     <div>
-      <h1>Pocket Codex!</h1>
-      <p>here are my {fecs.length} FECs</p>
-    </div>
-  )
+      <h1 className="center">Pocket Codex!</h1>
+      <div className="container">
+        <div className="row">
+        {fecs.map(({id, name, picture})=> (
+          <div className="col s6 m4" key={id}>
+            <div className="card horizontal">
+              <div className="card-image">
+                <img src={picture} alt={name}/>
+              </div>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <p>{name}</p>
+                </div>
+              </div>
+            </div>
+          </div>  
+        ))} </div>
+      </div>
+    </div>)
 }
 
 export default App;
 
-
+ 
